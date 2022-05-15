79612be4a0e154b2ee8047ebc585ce62ff8cd426
Rod Falanga <rfalanga@users.noreply.github.com>
2022-05-15 17:28:32 -0600

    some basic modifications
    Commented out the Privacy page, controller.
    Added both the Author and Title views and controllers.

---
+++ BookFinder/BookFinder/Controllers/AuthorController.cs
@@ -0,0 +1,22 @@
+﻿using BookFinder.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BookFinder.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly ILogger<AuthorController> _logger;
+
+        public AuthorController(ILogger<AuthorController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BookFinder/BookFinder/Controllers/TitleController.cs
@@ -0,0 +1,22 @@
+﻿using BookFinder.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BookFinder.Controllers
+{
+    public class TitleController : Controller
+    {
+        private readonly ILogger<TitleController> _logger;
+
+        public TitleController(ILogger<TitleController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
