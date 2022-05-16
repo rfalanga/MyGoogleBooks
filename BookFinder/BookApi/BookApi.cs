4afbe3ee18496cca25e0f069705926e0cfeb5276
Rod Falanga <rfalanga@users.noreply.github.com>
2022-05-15 18:19:27 -0600

    temporary commit

--- BookFinder/BookApi/BookApi.cs
+++ BookFinder/BookApi/BookApi.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookApi.Models;
 
 namespace BookApi
 {
     public class BookApi
     {
-
+        public async Root? GetBooksByAuthor(string authorNames)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    //this is an example of a generic search: https://www.googleapis.com/books/v1/volumes?q=christie
+                    //I need to find an example that searches specifically by author
+                }
+                catch (Exception)
+                {
+                    //an error has occurred, return an empty Root object
+                    var emptyRoot = new Root();
+                    return emptyRoot;
+                }
+            }
+        }
     }
 }
