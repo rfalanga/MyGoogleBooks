29f4af9a7714a06fd04bb0817389960f8bb39437
Rod Falanga <rfalanga@users.noreply.github.com>
2022-05-17 11:41:19 -0600

    trying different things

---
+++ BookFinder/BookFinder/Models/ViewModels/AuthorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookApi.Models;
+
+namespace BookFinder.Models.ViewModels
+{
+    public class AuthorViewModel
+    {
+        //public class BookFinder.Models.SimpleQuery SimpleQuery; //I still get a CS8618 warning here
+        public SimpleQuery SimpleQuery { get; set; }    // and CS8618 error here, too, but it is at least simplier
+    }
+}
