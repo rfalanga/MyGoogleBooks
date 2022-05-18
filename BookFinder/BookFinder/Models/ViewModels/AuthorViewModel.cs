652fa675ae3f3e6a01c86a113871a99f3e3aa900
Rod Falanga <rfalanga@users.noreply.github.com>
2022-05-17 20:24:53 -0600

    Got an input showing up to collect an author's name

--- BookFinder/BookFinder/Models/ViewModels/AuthorViewModel.cs
+++ BookFinder/BookFinder/Models/ViewModels/AuthorViewModel.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BookApi.Models;
-
-namespace BookFinder.Models.ViewModels
+﻿namespace BookFinder.Models.ViewModels
 {
     public class AuthorViewModel
     {
         //public class BookFinder.Models.SimpleQuery SimpleQuery; //I still get a CS8618 warning here
-        public SimpleQuery SimpleQuery { get; set; }    // and CS8618 error here, too, but it is at least simplier
+        //public SimpleQuery SimpleQuery { get; set; }    // and CS8618 error here, too, but it is at least simplier
+        public string AuthorName { get; set; }
+
+        public AuthorViewModel()
+        {
+            AuthorName = "";
+        }
     }
 }
